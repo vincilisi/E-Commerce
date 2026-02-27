81a3671d7fab05e9a3597165e5abd70f3a7d4fcd
Vincilisi <lisitano95@gmail.com>
2026-02-27 14:41:03 +0100

    speranza

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
 };
-
-const { withSentryConfig } = require('@sentry/nextjs');
-export default withSentryConfig(nextConfig, { silent: true });
 
 export default nextConfig;
